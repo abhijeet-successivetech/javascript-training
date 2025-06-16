64f5f9042c1221e5a92521a100bc1457fe8342a3
abhijeet gautam <abhijeet.gautam@successive.tech>
2025-06-16 18:44:14 +0530

    Completed the assignment

---
+++ Javascript Assignment day 9 (13-june-2025)/index.js
@@ -0,0 +1,8 @@
+//Q3. .create a new folder named 'math' and create a file math.js. In math.js, define two functions:
+// a. add(a, b): takes two numbers as arguments and returns their sum.
+// b. subtract(a, b): takes two numbers as arguments and returns their difference.
+// create a new file index.js and just export those functions and then use those function in any of the file
+
+const { Add, Subtract } = require("./math/math.js");
+
+console.log("The addition is "+Add(132, 554));
---
+++ Javascript Assignment day 9 (13-june-2025)/math/math.js
@@ -0,0 +1,11 @@
+// Exporting the functions required
+
+const Add = (num1, num2) => {
+  return num1 + num2;
+}
+
+const Subtract = (num1, num2) => {
+  return num1 - num2;
+}
+
+module.exports = { Add, Subtract };

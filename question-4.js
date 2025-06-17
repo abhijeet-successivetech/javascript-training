79275cc193b68be20bd59f3ec8d0351c9d0dccb2
abhijeet gautam <abhijeet.gautam@successive.tech>
2025-06-17 21:28:22 +0530

    Moved to root

--- question-4.js
+++
@@ -1,8 +0,0 @@
-// Write a function expression that takes in a number and returns its square.
-
-//ANS
-const square = function (number) {
-  return number ** 2;
-};
-
-console.log("The square of the number is", square(6));
--- question-5.js
+++
@@ -1,7 +0,0 @@
-// Write a function expression that takes in two numbers and returns their sum.
-
-//ANS
-const add = function (number1, number2) {
-  return number1 + number2;
-};
-console.log("The addition of two numbers is", add(1, 78));

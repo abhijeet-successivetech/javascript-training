ad1f502002a11c0ca150e4dccaee10a17891b52f
abhijeet gautam <abhijeet.gautam@successive.tech>
2025-06-18 15:57:54 +0530

    Revert "Completed the assignment"
    This reverts commit 64f5f9042c1221e5a92521a100bc1457fe8342a3.

--- Javascript Assignment day 9 (13-june-2025)/question-2.js
+++
@@ -1,6 +0,0 @@
-//Q2. Install the lodash package using npm install lodash. and use any of its function
-
-const lodash = require('lodash')
-const chunkArray = lodash.chunk([1,2,3,4],2); // Used a lodash functon chunk on an array
-
-console.log(chunkArray);

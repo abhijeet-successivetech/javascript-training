8d5bddc860ec2817ae43a79bb95e52e4e3e9e6f3
abhijeet gautam <abhijeet.gautam@successive.tech>
2025-06-18 17:36:40 +0530

    refactor: restored the previous files

---
+++ Javascript Assignment day 2(04-june-2025)/question-7.js
@@ -0,0 +1,34 @@
+// All of the above questions from 3-6 needs to be done with arrow functions also
+
+// (Using Arrow function) Write a function expression
+//  that takes in another function as an argument
+
+const firstFunction = () => {
+  console.log("Inside the first");
+};
+const secondFunction = (firstFunction) => {
+  firstFunction();
+  console.log("Inside the second");
+};
+secondFunction(firstFunction);
+
+// (Using Arrow function) Write a function expression that takes in a number
+//  and returns its square.
+
+const square = (number) => number ** 2;
+
+console.log("The square of the number is", square(33));
+
+// (Using Arrow function) Write a function expression that takes in two numbers
+//  and returns their sum.
+
+const sum = (number1, number2) => number1 + number2;
+
+console.log("The sum of two number is", sum(33, 43));
+
+// (Using Arrow function) Write a function expression that takes in a number
+// and returns true if it's even and false if it's odd.
+
+const isEven = (num) => (num % 2 == 0 ? true : false);
+
+console.log("The number is even", isEven(45));
